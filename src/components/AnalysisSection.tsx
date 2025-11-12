72a86883a4836cce64835b51683566e29e768711
RohanSunar15 <rohanksunar15@gmail.com>
2025-11-12 12:38:41 +0530

    added the priority level at right corner

--- src/components/AnalysisSection.tsx
+++ src/components/AnalysisSection.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "./ui/card";
 
 export function AnalysisSection() {
   return (
@@ -8,50 +14,81 @@ export function AnalysisSection() {
       </CardHeader>
       <CardContent>
         {/* Point Number 1 */}
-    
+
         <div className="p-2">
-           <Card>
-          <CardHeader className="text-left">
-            <CardTitle> Point Number 1</CardTitle>
-              <CardDescription className="text-sm text-gray-600">Description for Point Number 1</CardDescription>
-          </CardHeader>
-          <CardContent></CardContent>
-        </Card>
-        </div>
-       
-
-         {/* Point Number 2 */}
-
-         <div className="p-2">
           <Card>
-          <CardHeader className="text-left">
-            <CardTitle> Point Number 2</CardTitle>
-            <CardDescription className="text-sm text-gray-600">Description for Point Number 2</CardDescription>
-          </CardHeader>
-          <CardContent></CardContent>
-        </Card>
+            <CardHeader className="flex justify-between items-start">
+              <div className="text-left pb-5">
+                <CardTitle className="pb-2">Point Number 1</CardTitle>
+                <CardDescription className="text-sm text-gray-600 ">
+                  Description for Point Number 1
+                </CardDescription>
+              </div>
 
-         </div>
-        
-         {/* Point Number 3 */}
-        <div className="p-2"><Card className="">
-          <CardHeader className="text-left">
-            <CardTitle> Point Number 3</CardTitle>
-            <CardDescription className="text-sm text-gray-600">Description for Point Number 3</CardDescription>
-          </CardHeader>
-          <CardContent></CardContent>
-        </Card>
+              {/* Priority label on right side */}
+              <span className="text-xs font-medium text-green-600 bg-green-200 px-2 py-1 rounded-full">
+                Low Priority
+              </span>
+            </CardHeader>
+          </Card>
         </div>
 
+        {/* Point Number 2 */}
 
-         {/* Point Number 4 */}
-        <div className="p-2"><Card className="">
-          <CardHeader className="text-left">
-            <CardTitle> Point Number 4</CardTitle>
-            <CardDescription className="text-sm text-gray-600">Description for Point Number 4</CardDescription>
-          </CardHeader>
-          <CardContent></CardContent>
-        </Card>
+        <div className="p-2">
+          <Card>
+            <CardHeader className="flex justify-between items-start ">
+              <div className="text-left pb-5">
+                <CardTitle className="pb-2">Point Number 2</CardTitle>
+                <CardDescription className="text-sm text-gray-600 ">
+                  Description for Point Number 2
+                </CardDescription>
+              </div>
+
+              {/* Priority label on right side */}
+              <span className="text-xs font-medium text-green-600 bg-green-200 px-2 py-1 rounded-full">
+                Low Priority
+              </span>
+            </CardHeader>
+          </Card>
+        </div>
+
+        {/* Point Number 3 */}
+        <div className="p-2">
+          <Card className="">
+            <CardHeader className="flex justify-between items-start">
+              <div className="text-left pb-5">
+                <CardTitle className="pb-2">Point Number 3</CardTitle>
+                <CardDescription className="text-sm text-gray-600 ">
+                  Description for Point Number 3
+                </CardDescription>
+              </div>
+
+              {/* Priority label on right side */}
+              <span className="text-xs font-medium text-green-600 bg-green-200 px-2 py-1 rounded-full">
+                Low Priority
+              </span>
+            </CardHeader>
+          </Card>
+        </div>
+
+        {/* Point Number 4 */}
+        <div className="p-2">
+          <Card className="">
+            <CardHeader className="flex justify-between items-start">
+              <div className="text-left pb-5">
+                <CardTitle className="pb-2">Point Number 4</CardTitle>
+                <CardDescription className="text-sm text-gray-600 ">
+                  Description for Point Number 4
+                </CardDescription>
+              </div>
+
+              {/* Priority label on right side */}
+              <span className="text-xs font-medium text-green-600 bg-green-200 px-2 py-1 rounded-full">
+                Low Priority
+              </span>
+            </CardHeader>
+          </Card>
         </div>
       </CardContent>
     </Card>
